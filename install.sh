4baeb35ff70c4180a4e97002fcb2e87630385243
JOHN RHEG REGAYA <143176054+Rheg01@users.noreply.github.com>
2026-03-19 10:07:35 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 apt-get update
-apt-get install -y openjdk-21-jre wget unzip
+apt-get install -y default-jre wget curl unzip
 
-wget https://github.com/AsamK/signal-cli/releases/latest/download/signal-cli-x86_64.tar.gz
-tar xf signal-cli-x86_64.tar.gz
+echo "Downloading signal-cli..."
+curl -L https://github.com/AsamK/signal-cli/releases/latest/download/signal-cli-x86_64.tar.gz -o signal-cli.tar.gz
+
+echo "Extracting..."
+tar -xzf signal-cli.tar.gz
+
 mv signal-cli-* signal-cli
+chmod +x signal-cli/bin/signal-cli
+
+echo "Installation finished."
